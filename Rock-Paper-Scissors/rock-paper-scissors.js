0e441ec2e821f0ecdc86d3befc0636d658d1981f
Rebecca Parks <parksr412@gmail.com>
2023-02-19 18:50:28 -0500

    Add javascript

---
+++ Rock-Paper-Scissors/rock-paper-scissors.js
@@ -0,0 +1,5 @@
+const computerChoice = document.getElementById('computer-choice');
+const playerChoice = document.getElementById('player-choice');
+const resultDisplay = document.getElementById('result');
+
+const possibleChoices = document.querySelectorAll(".choice-button");

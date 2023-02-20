6aa6e89eb3362a4439dd73a0a7188b9893ec711f
Rebecca Parks <parksr412@gmail.com>
2023-02-19 19:52:31 -0500

    Add beginner version

--- Rock-Paper-Scissors/rock-paper-scissors.js
+++ Rock-Paper-Scissors/rock-paper-scissors.js
@@ -1,5 +1,58 @@
-const computerChoice = document.getElementById('computer-choice');
-const playerChoice = document.getElementById('player-choice');
-const resultDisplay = document.getElementById('result');
+const computerChoiceEl = document.getElementById("computer-choice");
+const playerChoiceEl = document.getElementById("player-choice");
+const resultEl = document.getElementById("result");
 
 const possibleChoices = document.querySelectorAll(".choice-button");
+let playerChoice
+let computerChoice
+let result
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener("click", (event) => {
+    playerChoice = event.target.id
+    playerChoiceEl.innerHTML = playerChoice
+    generateComputerChoice()
+    getResult()
+}))
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length)
+
+    if (randomNumber === 0){
+        computerChoice = "rock"
+    }
+
+    if (randomNumber === 1){
+        computerChoice = "paper"
+    }
+
+    if (randomNumber === 2){
+        computerChoice = "scissors"
+    }
+    computerChoiceEl.innerHTML = computerChoice
+}
+
+function getResult(){
+    if (computerChoice === playerChoice){
+        result = "TIE!!!!"
+    }
+
+    if (computerChoice === "rock" && playerChoice === "paper"){
+        result = "YOU WIN!!!!"
+    }
+    if (computerChoice === "rock" && playerChoice === "scissors"){
+        result = "YOU LOSE!!!!"
+    }
+    if (computerChoice === "paper" && playerChoice === "scissors"){
+        result = "YOU WIN!!!!"
+    }
+    if (computerChoice === "paper" && playerChoice === "rock"){
+        result = "YOU LOSE!!!!"
+    }
+    if (computerChoice === "scissors" && playerChoice === "paper"){
+        result = "YOU LOSE!!!!"
+    }
+    if (computerChoice === "scissors" && playerChoice === "rock"){
+        result = "YOU WIN!!!!"
+    }
+    resultEl.innerHTML = result
+}
